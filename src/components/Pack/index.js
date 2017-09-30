1109176526fff7c8f85b501282c129e0824c882b
Thomas Murphy <thomas.c.murphy@gmail.com>
2017-09-30 09:59:22 -0600

    think this fixes some issues with switching users

--- src/components/Pack/index.js
+++ src/components/Pack/index.js
@@ -11,36 +11,36 @@ class PackPage extends React.Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
-      pack: this.props.pack,
-      packCards: this.props.packCards,
-      deckCards: this.props.deckCards,
+      pack: {},
+      packCards: [],
+      deckCards: [],
       hash: this.props.hash,
       saving: false
     };
     this.savePick = this.savePick.bind(this);
   }
 
-  componentDidMount() {console.log('componentDidMount');
+  componentDidMount() {
     this.props.actions.loadPackCards(this.state.hash);
     this.props.actions.loadDeckCards(this.state.hash);
     this.props.actions.preloadImages(this.state.hash);
   }
 
-  componentWillReceiveProps(nextProps) {console.log('will receive props', this.props, nextProps);
+  componentWillReceiveProps(nextProps) {
     if (this.props.hash != nextProps.hash) {
-      console.log('receiveprops: player', nextProps);
+      //console.log('receiveprops: player', nextProps);
       this.setState({hash: nextProps.hash});
     }
     if (JSON.stringify(this.props.pack) != JSON.stringify(nextProps.pack)) {
-      console.log('receiveprops: load pack', nextProps);
+      //console.log('receiveprops: load pack', nextProps);
       this.setState({pack: nextProps.pack});
     }
     if (JSON.stringify(this.props.packCards) != JSON.stringify(nextProps.packCards)) {
-      console.log('receiveprops: load pack cards', this.props.packCards, nextProps.packCards);
+      //console.log('receiveprops: load pack cards', this.props.packCards, nextProps.packCards);
       this.setState({packCards: nextProps.packCards});
     }
     if (JSON.stringify(this.props.deckCards) != JSON.stringify(nextProps.deckCards)) {
-      console.log('receiveprops: load deck cards', this.props.deckCards, nextProps.deckCards);
+      //console.log('receiveprops: load deck cards', this.props.deckCards, nextProps.deckCards);
       this.setState({deckCards: nextProps.deckCards});
     }
   }
@@ -53,13 +53,13 @@ class PackPage extends React.Component {
 
 
   render() {
-    const packCards = this.props.packCards;
-    const deckCards = this.props.deckCards;
+    const packCards = this.state.packCards;
+    const deckCards = this.state.deckCards;
     var pack_title = 'Your Pack';
     var pick_title = '';
     var pack_card_list = <p>Waiting for your next pack to be passed</p>;
     if (this.props.pack.number > 0) {
-      pack_title = "Pack " + this.props.pack.number;
+      pack_title = "Pack " + this.state.pack.number;
       pick_title = <small>(Pick {(deckCards.length + 1)})</small>;
       pack_card_list = <PackCardList packCards={packCards} onClick={this.savePick} />;
     }
@@ -92,7 +92,6 @@ function collectPackCards(packCards, pack) {
       return packCard;
     }
   });
-  console.log('collect:', selected);
   return selected.filter(el => el != undefined);
 }
 
@@ -101,7 +100,6 @@ function mapStateToProps(state, ownProps) {
   let packCards = [];
   let deckCards = state.deckCards.length > 0 ? state.deckCards : [];
   const hash = ownProps.params.hash;
-  console.log('state', state);
   if (state.packs.length > 0 && state.packCards.length > 0) {
     let packId = state.packCards[0].pack_id;
     pack = Object.assign({}, state.packs.find(pack => pack ? pack.id == packId : false));
